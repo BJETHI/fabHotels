d7db6d3901c46ac2f95e385efb3dcccb16c78c39
bjethi <bikramjethi@gmail.com>
2019-03-17 12:15:47 +0530

    Category Slide Component added

--- src/organisms/CategorySlides/CategorySlides.js
+++ src/organisms/CategorySlides/CategorySlides.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+import CategorySlide from '../../molecules/CategorySlide/CategorySlide';
+
 class CategorySlides extends Component {
+
+    renderCategorySlide = (child) => {
+        const { data } = child;
+        return <CategorySlide slideData={data} />;
+    }
+
     render() {
+        const { categoryData } = this.props;
+        const children = categoryData && categoryData.children;
         return (
             <div>
-                CategorySlides
+                {children && children.map(child => this.renderCategorySlide(child))}
             </div>
         )
     }
 }
 
 const mapStateToProps = state => ({
-    categoryData: state.categoryData,
+    categoryData: state.categoryData.categoryData,
 });
 
 export default connect(mapStateToProps, null)(CategorySlides)
